dc374648dfb0614bdafacab9c3523579f22f0f5e
flaviosilva <andrei.flavio@hotmail.com>
2023-05-04 11:27:43 -0300

    Implemented process to detect objects on images

--- api/models.py
+++ api/models.py
@@ -1,6 +1,11 @@
 from typing import Union
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+
 
 class Image(BaseModel):
     content: str
     minimum_possibility: Union[int, None] = None
+
+    @validator("content")
+    def replace_prefix(cls, v):
+        return v.replace("data:image/jpeg;base64,", "")
--- api/service.py
+++ api/service.py
@@ -1,29 +1,37 @@
 import os
+from datetime import datetime
+import base64
 from imageai.Detection import ObjectDetection
 from .models import Image
 
 
 class ImageHandler:
-    def create_temp_image(self):
-        pass
+    """
+    Class to do all operations with images
+    """
 
-    def remove_temp_image(self):
-        pass
+    def create_temp_image(self, imageb64: str, target_folder: str) -> str:
+        imgdata = base64.b64decode(imageb64)
+        filename = target_folder + "/image{:%Y%m%d%H%M%S}.jpg".format(datetime.now())
+        with open(filename, "wb") as f:
+            f.write(imgdata)
+        return filename
 
     def find_objects(self, image: Image):
-        pass
+        execution_path = os.getcwd()
+        image_name = self.create_temp_image(
+            image.content, os.path.join(execution_path, "api", "images_processed")
+        )
 
-        # execution_path = os.getcwd()
-
-        # detector = ObjectDetection()
-        # detector.setModelTypeAsYOLOv3()
-        # detector.setModelPath(os.path.join(execution_path, "yolov3.pt"))
-        # detector.loadModel()
-        # detections = detector.detectObjectsFromImage(
-        #     input_image=os.path.join(execution_path, "image2.jpg"),
-        #     output_image_path=os.path.join(execution_path, "image2new.jpg"),
-        #     minimum_percentage_probability=30,
-        # )
+        detector = ObjectDetection()
+        detector.setModelTypeAsYOLOv3()
+        detector.setModelPath(os.path.join(execution_path, "yolov3.pt"))
+        detector.loadModel()
+        detections = detector.detectObjectsFromImage(
+            input_image=image_name,
+            output_image_path="{}_new.jpg".format(image_name[: image_name.find(".")]),
+            minimum_percentage_probability=image.minimum_possibility or 50,
+        )
 
         # for eachObject in detections:
         #     print(
